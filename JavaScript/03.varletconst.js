2deb49c2bce98aa77a250675646feb3271bc37fd
Johannes Feige <johannes.feige@netzkern.de>
2020-03-13 15:35:39 +0100

    changes

--- JavaScript/03.varletconst.js
+++ JavaScript/03.varletconst.js
@@ -1,14 +1,19 @@
 var foo = 'first';
+
 function func() {
   var foo = 'second';
   console.log(foo);
 }
 
 console.log(foo);
+
 func();
+
 console.log(foo);
+
 if (true) {
   var foo = 'third';
   console.log(foo);
 }
+
 console.log(foo);
--- JavaScript/04.Functions.js
+++ JavaScript/04.Functions.js
@@ -1,47 +1,35 @@
-// #region statement
 function printStatement(message) {
   console.log(message);
 
   return {
-    result: 'success'
+    result: 'success',
   };
 }
 
 printStatement('foo statement');
 
-//#endregion
-
-// #region expression
 var printExpression = function(message) {
   console.log(message);
 
   return {
-    result: 'success'
+    result: 'success',
   };
 };
 
 printExpression('foo expression');
 
-// #endregion
-
-// #region anonymous
+// anonym
 setTimeout(function() {
   console.log('done');
 }, 1000);
 
-//#endregion
-
-// #region misc
 // Funktionen können Eigenschaften haben:
-
 printStatement.myProperty = 42;
 
 console.log(printStatement.myProperty);
 
-//#endregion
-
-// function sayHello2(name) {
-//   console.log(`Hello ${name}!`);
-// }
-// sayHello2('Team Microsoft Solutions', 'hallo');
-// sayHello2();
+function sayHello2(name) {
+  console.log(`Hello ${name}!`);
+}
+sayHello2('Team Microsoft Solutions', 'hallo');
+sayHello2();

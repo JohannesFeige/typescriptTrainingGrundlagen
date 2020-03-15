0821c4c6f6d56e3cb9df178b8668039fbc801f24
Johannes <johannesfeige@gmail.com>
2020-03-15 19:24:41 +0100

    wording

--- JavaScript/05.02.Hoisting.js
+++ JavaScript/05.02.Hoisting.js
@@ -1,6 +1,6 @@
 // -- Function hoisting
 // die Funktion wird an den Anfang des aktuellen Scopes verschoben:
-// sayHello('Team Microsoft Solutions');
+// sayHello('Team Fixit');
 // function sayHello(name) {
 //   console.log(`Hello ${name}!`);
 // }
@@ -8,10 +8,10 @@
 // function sayHello2(name) {
 //   console.log(`Hello ${name}!`);
 // }
-// sayHello2('Team Microsoft Solutions');
+// sayHello2('Team Fixit');
 
 // // nicht jedoch bei function expressions; auch nicht bei var:
-// sayHello('Team Microsoft Solutions');
-// const sayHello = function(name) {
+// sayHello('Team Fixit');
+// var sayHello = function(name) {
 //   console.log(`Hello ${name}!`);
 // };
